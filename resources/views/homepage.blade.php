3baa46d7b020c2ed155268cd649a7a6b8a8ca264
syzwnongithub <syazwan200600@gmail.com>
2021-06-15 20:57:23 +0800

    Amir-repair

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,10 +15,8 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     @auth
                     You're logged in as a user!
-                    <button type="button">Save Preset</button>
                     @else
                     You're a guest.
-                    <button type="button" disabled>Save Preset</button>
                     @endauth
                 </div>
             </div>
